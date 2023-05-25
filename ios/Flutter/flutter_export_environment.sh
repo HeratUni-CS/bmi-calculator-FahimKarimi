f83fdce6835f50c073e36679d65df2fa85493433
FahimKarimi <129143505+FahimKarimi@users.noreply.github.com>
2023-05-25 23:18:00 +0430

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\FAHIM KARIMI\Desktop\bmi-calculator-FahimKarimi-main"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Ab.Rouf\Documents\first1\BMI_calculator"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
